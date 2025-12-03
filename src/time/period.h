0a44f9bf68de204f5cab8ee99005e63ae76a298b
Ryan Potter <ryan.potter94@gmail.com>
2025-12-03 14:56:16 +0000

    Added periods and frequencies for reference.

---
+++ src/time/period.h
@@ -0,0 +1,23 @@
+#pragma once
+
+enum class Period
+{
+    Year         = 365,
+    LeapYear     = 366,
+    January      = 31,
+    February     = 28,
+    LeapFeburary = 29,
+    March        = 31,
+    April        = 30,
+    May          = 31,
+    June         = 30,
+    July         = 31,
+    August       = 31,
+    September    = 31,
+    October      = 31,
+    November     = 30,
+    December     = 31,
+    Fortnightly  = 14,
+    Week         = 7,
+    Daily        = 1
+};
