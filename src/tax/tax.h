e21ffa176ad91b6dff18522645d9ffcf6bbfa18e
Ryan Potter <ryan.potter94@gmail.com>
2025-12-04 12:15:17 +0000

    Moved tax.h to pennies

--- src/tax/tax.h
+++ src/tax/tax.h
@@ -1,9 +1,10 @@
 #pragma once
 #include <string>
+#include <cstdint>
 
 class Tax{
     public:
         virtual ~Tax() = default;
-        virtual float calculateTax(float amount) const = 0;
+        virtual int64_t calculateTax(int64_t amount) const = 0;
         std::string name = "Tax";
 };
