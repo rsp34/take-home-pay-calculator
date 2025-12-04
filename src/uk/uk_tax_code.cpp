1abbac61b9d879f2bbc258ae2cdf62ca9f0e7081
Ryan Potter <ryan.potter94@gmail.com>
2025-12-04 09:13:32 +0000

    Removed rawcode from UKTaxCode because we don't need it after initialisation.

--- src/uk/uk_tax_code.cpp
+++ src/uk/uk_tax_code.cpp
@@ -1,25 +1,25 @@
 #include "./uk_tax_code.h"
 
 // TaxCode
-std::string validateTaxCode(std::string rawCode)
+std::string validateTaxCode(const std::string &code)
 {
-    if (rawCode.size() != 5){
+    if (code.size() != 5){
         throw std::invalid_argument("UK tax codes are five characters in length");
-    };
+    }
         
     for (int i = 0; i < 4; ++i){
-        if (!std::isdigit(rawCode[i])){
+        if (!std::isdigit(code[i])){
             throw std::invalid_argument("UK tax codes begin with four digits.");
-        };
+        }
     }
 
-    char letter = rawCode.back();
+    char letter = code.back();
     std::string validLetters = "LMPTY";
     if (validLetters.find(letter) == std::string::npos){
         throw std::invalid_argument("UK tax codes must end with one of " + validLetters);
-    };
+    }
 
-    return rawCode;
+    return code;
 
 }
 
--- src/uk/uk_tax_code.h
+++ src/uk/uk_tax_code.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <stdexcept>
+#include <cctype>
 
 int readPersonalAllowance(const std::string &code);
 char readCategoryLetter(const std::string &code);
@@ -9,11 +10,8 @@ std::string validateTaxCode(const std::string &code);
 class UKTaxCode
 {
 public:
-    UKTaxCode(const std::string& code) : rawCode{validateTaxCode(code)}, personalAllowance(readPersonalAllowance(code) * 10), categoryLetter(readCategoryLetter(code)) {};
+    UKTaxCode(const std::string &code) : personalAllowance(readPersonalAllowance(code) * 10), categoryLetter(readCategoryLetter(code)) {};
 
-    float personalAllowance;
-    char categoryLetter;
-
-private:
-    std::string rawCode;
+    const float personalAllowance;
+    const char categoryLetter;
 };
