8f545c19d8a25bd2621d1ba19d2af721840c990b
Ryan Potter <ryan.potter94@gmail.com>
2025-12-09 16:44:24 +0000

    Some updates to payItem factories.

---
+++ src/uk/uk_pay_item_factory.cpp
@@ -0,0 +1,28 @@
+#include "uk_pay_item_factory.h"
+#include "../pay/pay_item.h"
+
+PayItem UKPayItemFactory::createSalary(int64_t amount)
+{
+    PayItem payItem = PayItem("Salary", amount, EffectType::TaxableAddition, false);
+    payItem.applicableTaxes_ = {UKTaxNames::EMPLOYEE_NI, UKTaxNames::INCOME_TAX};
+    return payItem;
+}
+
+PayItem createEmployerPensionContribution(int64_t amount, int64_t percentage)
+{
+    return PayItem("Employer Pension Contribution", amount * percentage/100, EffectType::NonTaxableAddition, false);
+}
+
+PayItem createEmployeePensionContribution(float amount, float percentage)
+{
+    PayItem payItem = PayItem("Employee Pension Contribution", amount * percentage, EffectType::PreTaxDeduction, false);
+    payItem.applicableTaxes_ = {UKTaxNames::EMPLOYEE_NI, UKTaxNames::INCOME_TAX};
+    return payItem;
+}
+
+PayItem createBonus(float amount)
+{
+    PayItem payItem = PayItem("Bonus", amount, EffectType::TaxableAddition, true);
+    payItem.applicableTaxes_ = {UKTaxNames::EMPLOYEE_NI, UKTaxNames::INCOME_TAX};
+    return payItem;
+}
--- src/uk/uk_pay_item_factory.h
+++ src/uk/uk_pay_item_factory.h
@@ -1,34 +1,11 @@
 #pragma once
-#include "../pay/pay_item.h"
-#include "../uk/uk_tax_model.h"
+#include "uk_tax_names.h"
+
+class PayItem;
 
 class UKPayItemFactory
 {
-    static PayItem createSalary(float amount, std::shared_ptr<UKIncomeTaxModel> incomeTax, std::shared_ptr<UKNIModel> nationalInsurance);
-    static PayItem createPension(float amount);
-    static PayItem createBonus(float amount);
+    static PayItem createSalary(int64_t amount);
+    static PayItem createPension(int64_t amount);
+    static PayItem createBonus(int64_t amount);
 };
-
-PayItem createSalary(float amount, std::shared_ptr<UKIncomeTaxModel> incomeTax, std::shared_ptr<UKNIModel> nationalInsurance)
-{
-    PayItem payItem = PayItem("Salary", amount, EffectType::TaxableAddition, false);
-    payItem.taxModels = {incomeTax, nationalInsurance};
-    return payItem;
-}
-
-PayItem createEmployerPensionContribution(float amount, float percentage)
-{
-    return PayItem("Employer Pension Contribution", amount * percentage, EffectType::NonTaxableAddition, false);
-}
-
-PayItem createEmployeePensionContribution(float amount, float percentage)
-{
-    return PayItem("Employee Pension Contribution", amount * percentage, EffectType::PreTaxDeduction, false);
-}
-
-PayItem createBonus(float amount, std::shared_ptr<UKIncomeTaxModel> incomeTax, std::shared_ptr<UKNIModel> nationalInsurance)
-{
-    PayItem payItem = PayItem("Bonus", amount, EffectType::TaxableAddition, true);
-    payItem.taxModels = {incomeTax, nationalInsurance};
-    return payItem;
-}
