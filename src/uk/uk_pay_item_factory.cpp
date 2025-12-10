1d64a12ae99817237a2cfc4465f3e6e97dfb0440
Ryan Potter <ryan.potter94@gmail.com>
2025-12-10 09:15:10 +0000

    Fixed factory signatures and updated h file.

--- src/uk/uk_pay_item_factory.cpp
+++ src/uk/uk_pay_item_factory.cpp
@@ -13,14 +13,14 @@ PayItem createEmployerPensionContribution(int64_t amount, int64_t percentage)
     return PayItem("Employer Pension Contribution", amount * percentage/100, EffectType::NonTaxableAddition, false);
 }
 
-PayItem createEmployeePensionContribution(float amount, float percentage)
+PayItem createEmployeePensionContribution(int64_t amount, int64_t percentage)
 {
-    PayItem payItem = PayItem("Employee Pension Contribution", amount * percentage, EffectType::PreTaxDeduction, false);
+    PayItem payItem = PayItem("Employee Pension Contribution", amount * percentage/100, EffectType::PreTaxDeduction, false);
     payItem.applicableTaxes_ = {UKTaxNames::EMPLOYEE_NI, UKTaxNames::INCOME_TAX};
     return payItem;
 }
 
-PayItem createBonus(float amount)
+PayItem createBonus(int64_t amount)
 {
     PayItem payItem = PayItem("Bonus", amount, EffectType::TaxableAddition, true);
     payItem.applicableTaxes_ = {UKTaxNames::EMPLOYEE_NI, UKTaxNames::INCOME_TAX};
--- src/uk/uk_pay_item_factory.h
+++ src/uk/uk_pay_item_factory.h
@@ -6,6 +6,7 @@ class PayItem;
 class UKPayItemFactory
 {
     static PayItem createSalary(int64_t amount);
-    static PayItem createPension(int64_t amount);
+    static PayItem createEmployerPensionContribution(int64_t amount, int64_t percentage);
+    static PayItem createEmployeePensionContribution(int64_t amount, int64_t percentage);
     static PayItem createBonus(int64_t amount);
 };
